9c4d57c9f5915862d1c7ec47a4a8183af6c2b8da
Muhammad Syawaludin <syawalhastaf@gmail.com>
2020-10-24 19:01:46 +0800

    tidy up material ui and css

---
+++ components/ChannelList.js
@@ -0,0 +1,58 @@
+import React, { memo } from "react";
+import styles from '../styles/Channel.module.css';
+import { Grid, Typography } from '@material-ui/core';
+
+const ChannelList = ({ data }) => {
+  // console.log('render');
+  return (
+    data.length === 0 ? <Grid container justify="center"><Typography>No Channel Found</Typography></Grid> :
+      data.map((obj, index) => {
+        return (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <div className={styles.card}>
+              <div className={styles.header}>
+                <div className={styles.imgWrapper}>
+                  <img className={styles.channellogo} src={obj.imageUrl} />
+                </div>
+                <div className={styles.channelWrapper}>
+                  <p>CH{obj.stbNumber}</p>
+                  <p className={styles.channelTitle}>{obj.title}</p>
+                </div>
+              </div>
+              <hr className={styles.borderTop} />
+              <div className={styles.channelDetails}>
+                {
+                  obj.currentSchedule.slice(0, 3).map((schedule, i) => {
+
+                    let timeStamp = Date.parse(schedule.datetime);
+                    let dateObject = new Date(timeStamp);
+
+                    const scheduleTime = dateObject.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', hour12: true });
+
+                    return (
+                      <div className={styles.schedule} key={i}>
+                        {schedule.title == 'Not Available' ?
+                          <>
+                            <span className={i == 0 ? styles.timeActive : styles.time}>N/A</span>
+                            <span className={i == 0 ? styles.timeActive : styles.time}>No Information Available</span>
+                          </>
+                          :
+                          <>
+                            <span className={i == 0 ? styles.timeActive : styles.time}>{i == 0 ? 'On Now' : scheduleTime}</span>
+                            <span className={i == 0 ? styles.scheduleActive : styles.scheduleTitle}>{schedule.title}</span>
+                          </>
+                        }
+
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </Grid>
+        )
+      })
+  )
+};
+
+export default memo(ChannelList);
--- pages/index.js
+++ pages/index.js
@@ -2,68 +2,7 @@ import Head from 'next/head';
 import React, { useEffect, useState, memo, useCallback } from "react";
 import styles from '../styles/Home.module.css';
 import { Button, Container, Grid, SwipeableDrawer, Typography, TextField } from '@material-ui/core';
-
-const people = [
-  "Siri",
-  "Alexa",
-  "Google",
-  "Facebook",
-  "Twitter",
-  "Linkedin",
-  "Sinkedin"
-];
-
-const ChannelList = memo(({ data }) => {
-  console.log('rendered: channelList');
-  return (
-    data.map((obj, index) => {
-      return (
-        <Grid item xs={12} sm={6} md={4} key={index}>
-          <div className={styles.card}>
-            <div className={styles.header}>
-              <div className={styles.imgWrapper}>
-                <img className={styles.channellogo} src={obj.imageUrl} />
-              </div>
-              <div className={styles.channelWrapper}>
-                <p>CH{obj.stbNumber}</p>
-                <p className={styles.channelTitle}>{obj.title}</p>
-              </div>
-            </div>
-            <hr className={styles.borderTop} />
-            <div className={styles.channelDetails}>
-              {
-                obj.currentSchedule.slice(0, 3).map((schedule, i) => {
-
-                  let timeStamp = Date.parse(schedule.datetime);
-                  let dateObject = new Date(timeStamp);
-
-                  const scheduleTime = dateObject.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', hour12: true });
-
-                  return (
-                    <div className={styles.schedule} key={i}>
-                      {schedule.title == 'Not Available' ?
-                        <>
-                          <span className={i == 0 ? styles.timeActive : styles.time}>N/A</span>
-                          <span className={i == 0 ? styles.timeActive : styles.time}>No Information Available</span>
-                        </>
-                        :
-                        <>
-                          <span className={i == 0 ? styles.timeActive : styles.time}>{i == 0 ? 'On Now' : scheduleTime}</span>
-                          <span className={i == 0 ? styles.scheduleActive : styles.scheduleTitle}>{schedule.title}</span>
-                        </>
-                      }
-
-                    </div>
-                  )
-                })
-              }
-            </div>
-          </div>
-        </Grid>
-      )
-    })
-  )
-});
+import ChannelList from '../components/ChannelList';
 
 const Home = ({ data }) => {
 
@@ -154,7 +93,9 @@ const Home = ({ data }) => {
           <form>
             <TextField
               type="text"
-              placeholder="Search"
+              variant="outlined"
+              label="Search Channel Name"
+              placeholder="Type here ..."
               value={searchTerm}
               onChange={handleChange}
             />
@@ -169,7 +110,12 @@ const Home = ({ data }) => {
           <Button onClick={() => handleReset()} variant="outlined">reset</Button>
           <Button onClick={() => handleSortByName()} variant="outlined">{newData}</Button>
         </div> */}
-        <Grid container spacing={2} item>
+        <Grid
+          container
+          spacing={2}
+          item
+          // justify="center"
+        >
           <ChannelList data={channelData} />
         </Grid>
         <footer className={styles.footer}>
