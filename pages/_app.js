02c50d4d96650790da6006cdb0238a39dcd745de
Muhammad Syawaludin <syawalhastaf@gmail.com>
2020-10-26 00:10:28 +0800

    add toggle dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { createMuiTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 import '../styles/globals.css';
+import { themeAtom } from '../src/atoms';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+
+
+const ThemeWrapper = ({ children }) => {
+  const themeValue = useRecoilValue(themeAtom);
+  const [theme, setTheme] = useState({});
+  useEffect(() => {
+    let newObject = {
+      palette: {
+        type: themeValue,
+        primary: {
+          main: '#E6007D',
+        },
+        secondary: {
+          main: '#19857b',
+        },
+        error: {
+          main: red.A400,
+        },
+      },
+    }
+    setTheme(newObject);
+  }, [themeValue]);
+  const themeConfig = createMuiTheme(theme);
+  return (
+    <MuiThemeProvider theme={themeConfig}>{children}</MuiThemeProvider>
+  )
+}
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
-
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-          primary: {
-            main: '#E6007D',
-          },
-          secondary: {
-            main: '#19857b',
-          },
-          error: {
-            main: red.A400,
-          },
-        },
-      }),
-    [prefersDarkMode],
-  );
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -42,15 +48,17 @@ export default function MyApp(props) {
 
   return (
     <React.Fragment>
-      <Head>
-        <title>Astro Channels</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <RecoilRoot>
+        <Head>
+          <title>Astro Channels</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeWrapper>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeWrapper>
+      </RecoilRoot>
     </React.Fragment>
   );
 }
