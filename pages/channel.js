a1daf661fd8e7a38e3acd6b27174d5089484de73
Muhammad Syawaludin <syawalhastaf@gmail.com>
2020-10-25 08:32:11 +0800

    add schedule list

--- pages/channel.js
+++ pages/channel.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
-import { Container } from '@material-ui/core';
+import { Container, Tabs, Tab } from '@material-ui/core';
+import TabPanel from '../components/TabPannel';
 import styles from '../styles/Details.module.css';
 
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     padding: '2px 4px',
     display: 'flex',
     alignItems: 'center',
   },
+  tabWidth: {
+    minWidth: 50
+  }
 }));
 
 const ChannelDetails = ({ data }) => {
 
-  const theme = useTheme();
+  const classes = useStyles();
+  const scheduleDate = Object.keys(data.schedule);
+  const scheduleList = Object.values(data.schedule);
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
 
   return (
     <main>
@@ -38,6 +51,51 @@ const ChannelDetails = ({ data }) => {
           </div>
         </div>
         <p>{data.description}</p>
+
+        {/* {console.log('data', scheduleList[value])} */}
+
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          className={classes.tabWidth}
+          variant="standard"
+        >
+          {scheduleDate.map((obj, index) => {
+            let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            const timeStamp = Date.parse(obj);
+            let dateObject = new Date(timeStamp);
+            const days = daysOfWeek[dateObject.getDay()];
+            let currentDate = new Date();
+            const today = daysOfWeek[currentDate.getDay()]
+            return (
+              <Tab
+                key={index}
+                classes={{ root: classes.tabWidth }}
+                label={days == today ? 'Today' : days}
+              />
+            )
+          })}
+        </Tabs>
+        {/* {console.log('check item', scheduleList)} */}
+        {scheduleList.map((schedule, tabIndex) => {
+
+          return (
+            <TabPanel key={tabIndex} value={value} index={tabIndex}>
+              <ul>
+                {schedule.map((item, i) => {
+                  let timeStamp = Date.parse(item.datetime);
+                  let dateObject = new Date(timeStamp);
+                  const scheduleTime = dateObject.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', hour12: true });
+                  return (
+                    <li style={{ listStyleType: 'none' }} key={i}>{scheduleTime + '  ' + item.title}</li>
+                  )
+                })}
+              </ul>
+            </TabPanel>
+          )
+        })}
       </Container>
     </main>
   );
